0e69de1a187c131c4d51c2d3dc59f3e9a46707dc
Andreas Heizenreder <aheizenreder@prodyna.com>
2014-07-24 12:06:15 +0200

    fixed MemberRegistrationTest.

--- pac-ressys-ejb/pac-ressys-ejb-impl/src/test/java/com/prodyna/pac/ressys/pac/ressys/test/MemberRegistrationTest.java
+++ pac-ressys-ejb/pac-ressys-ejb-impl/src/test/java/com/prodyna/pac/ressys/pac/ressys/test/MemberRegistrationTest.java
@@ -25,30 +25,28 @@ import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.prodyna.pac.ressys.pac.ressys.model.Member;
-import com.prodyna.pac.ressys.pac.ressys.service.MemberRegistrationImpl;
-import com.prodyna.pac.ressys.util.Resources;
+import com.prodyna.pac.ressys.pac.ressys.service.MemberRegistration;
+import com.prodyna.pac.ressys.test.Deployments;
 
 @RunWith(Arquillian.class)
 public class MemberRegistrationTest {
     @Deployment
     public static Archive<?> createTestArchive() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClasses(Member.class, MemberRegistrationImpl.class, Resources.class)
-                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                // Deploy our test datasource
-                .addAsWebInfResource("test-ds.xml", "test-ds.xml");
+    	return Deployments.createDeployment();
+//        return ShrinkWrap.create(WebArchive.class, "test.war")
+//                .addClasses(Member.class, MemberRegistrationImpl.class, Resources.class)
+//                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+//                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+//                // Deploy our test datasource
+//                .addAsWebInfResource("test-ds.xml", "test-ds.xml");
     }
 
     @Inject
-    MemberRegistrationImpl memberRegistration;
+    MemberRegistration memberRegistration;
 
     @Inject
     Logger log;
